531dde7bf1f5799ca9dd86091cba69f76d68a925
mohamedabotyear <mohamedabotyear@gmail.com>
2023-03-23 21:14:10 +0200

    add response pong

--- src/Server.cs
+++ src/Server.cs
@@ -4,15 +4,28 @@ using System.Text;
 
 var ipAddress = new IPEndPoint(IPAddress.Any, 6379);
 TcpListener tcp = new(ipAddress);
+byte[] data  ;
+string result = "";
+var windowSize = 0;
 try
 {
 	tcp.Start();
 	using TcpClient client= await tcp.AcceptTcpClientAsync();
     await using NetworkStream stream = client.GetStream();
-    var message = $"📅 {DateTime.Now} 🕛";
+    windowSize = stream.ReadByte();
+    var message = $"" ;
+    if (windowSize != 0)
+    {
+
+    data = new byte[windowSize];
+    await stream.ReadAsync(data);
+      //result = string.Join(" ", data.ToArray());
+       // message += $"{Encoding.UTF8.GetString(data)}";
+        message+= "+PONG\r\n";
+    }
     var dateTimeBytes = Encoding.UTF8.GetBytes(message);
     await stream.WriteAsync(dateTimeBytes);
-
+    Console.ReadKey();
     Console.WriteLine($"Sent message: \"{message}\"");
 }
 catch (Exception)
