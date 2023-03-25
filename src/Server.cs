97b5ebd0f027707ba1775d51b078b7333f795f81
mohamedabotyear <mohamedabotyear@gmail.com>
2023-03-26 01:49:01 +0200

    global exception

--- src/Server.cs
+++ src/Server.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.IO;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -31,35 +32,37 @@ finally
 
 
 static async Task handleClientAsync(TcpClient client)
+
+{ 
+
+        while (true)
 {
+
+        NetworkStream stream = client.GetStream();
     try
     {
 
 
-
-        while (true)
+        var windowSize = stream.ReadByte();
+        if (windowSize != 0)
         {
 
-            NetworkStream stream = client.GetStream();
-
-            var windowSize = stream.ReadByte();
-            if (windowSize != 0)
-            {
-
-                var message = $"";
+            var message = $"";
 
 
-                message += "+PONG\r\n";
-                var dateTimeBytes = Encoding.UTF8.GetBytes(message);
-                await stream.WriteAsync(dateTimeBytes);
-            }
+            message += "+PONG\r\n";
+            var dateTimeBytes = Encoding.UTF8.GetBytes(message);
+            await stream.WriteAsync(dateTimeBytes);
         }
     }
     catch (Exception)
     {
-        Console.WriteLine("-Error");
+        var dateTimeBytes = Encoding.UTF8.GetBytes("-Error");
+        await stream.WriteAsync(dateTimeBytes);
         throw;
-    }
+    } 
+}
+        
 }
 
 
