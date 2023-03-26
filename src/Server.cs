11d6a6779ddba145097bfc43f7af865d705051dd
mohamedabotyear <mohamedabotyear@gmail.com>
2023-03-26 03:52:50 +0200

    F

--- src/Server.cs
+++ src/Server.cs
@@ -13,7 +13,7 @@ try
 
         //new Thread(new ThreadStart(async() =>
         //{
-        Task.Run(async () => await handleClientAsync(await tcp.AcceptTcpClientAsync()));
+        Task.Run(async () =>  HandleClient(await tcp.AcceptTcpClientAsync()));
         //})).Start();
     }
 
@@ -30,42 +30,73 @@ finally
 }
 
 
-static async Task handleClientAsync(TcpClient client)
-{
-    try
-    {
-        client.ReceiveTimeout= 0;
-        client.SendTimeout= 0;
 
+
+    static void HandleClient(TcpClient client)
+    {
+        NetworkStream stream = client.GetStream();
+
+        byte[] buffer = new byte[1024];
+        int bytesRead;
 
         while (true)
         {
-             NetworkStream stream = client.GetStream();
+            bytesRead = 0;
 
-            var windowSize = stream.ReadByte();
-            if (windowSize != 0)
+            try
+            {
+                // Read from the stream
+                bytesRead = stream.Read(buffer, 0, 1024);
+            }
+            catch
+            {
+                // A socket error has occurred
+                break;
+            }
+
+            if (bytesRead == 0)
+            {
+                // The client has disconnected from the server
+                break;
+            }
+
+
+
+            // Convert the data received into a string
+
+
+
+
+            var encoder = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            var data = string.Join("", encoder.ToArray());
+
+            if (data.Contains("ECHO"))
+            {
+                var index = data.IndexOf("ECHO");
+                var echoStart = data[index];//+5+4
+                var INDEX = data.Length - (index + 10);
+                var echoedMessage = data.Substring(index + 10, INDEX);
+                var result = echoedMessage.Replace("\r\n", "").Replace("\0", "");
+                var message = $"${result.Length}\r\n{result}\r\n";
+                if (stream.CanWrite)
+                    stream.Write(Encoding.UTF8.GetBytes(message), 0, Encoding.UTF8.GetBytes(message).Length);
+            }
+            else
             {
 
-                var message = $"";
 
-
-                message += "+PONG\r\n";
-                var dateTimeBytes = Encoding.UTF8.GetBytes(message);
-              await  stream.WriteAsync(dateTimeBytes);
+                // Echo the data back to the client
+                byte[] dataSent = Encoding.UTF8.GetBytes("+PONG\r\n");
+                stream.Write(dataSent, 0, dataSent.Length);
             }
-           
         }
-    }
-    catch (Exception)
-    {
 
-        throw;
-    }finally
-    {
-        client.Close(); 
-        client.Dispose();
+        // Clean up the client connection
+        client.Close();
+        Console.WriteLine("Client disconnected.");
     }
-} 
+
+
 
 
 
